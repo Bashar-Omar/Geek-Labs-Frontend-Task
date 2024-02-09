934f37528fd767c3b49971e8bbadb4aced955304
Bashar <bashar.omar.fd@gmail.com>
2024-02-10 00:04:30 +0200

    fix the URL images

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,15 @@
 /** @format */
-
+import Logo from "../assets/logo.png";
+import SearchIcon from "../assets/search-icon.svg";
+import Profile from "../assets/profile.png";
+import ArrowIcon from "../assets/Arrow.svg";
 const Header = () => {
   return (
     <header>
       <div className="container mx-auto py-5">
         <div className="flex justify-between items-center">
           <img
-            src="/src/assets/logo.png"
+            src={Logo}
             alt="logo"
             className="logo mx-10 h-14 cursor-pointer"
           />
@@ -18,13 +21,13 @@ const Header = () => {
             />
             <div className="flex text-dim-textAccSec items-center">
               <img
-                src="/src/assets/search-icon.svg"
-                alt="logo"
+                src={SearchIcon}
+                alt="search-icon"
                 className="search-icon w-8 h-8 cursor-pointer"
               />
               <div className="profile flex justify-center">
                 <img
-                  src="/src/assets/profile.png"
+                  src={Profile}
                   alt="profile"
                   className="profile-img rounded-full w-12 h-12 mx-5 cursor-pointer"
                 />
@@ -34,7 +37,7 @@ const Header = () => {
                 </div>
               </div>
               <img
-                src="/src/assets/Arrow.svg"
+                src={ArrowIcon}
                 alt="Arrow"
                 className="arrow-icon w-8 h-8 m-3 cursor-pointer"
               />
